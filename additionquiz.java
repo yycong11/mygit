9af37b5ffa1e348e896a9a0089dbeff163db0238
yycong11 <54837528+yycong11@users.noreply.github.com>
2019-09-17 16:14:09 +0800

    Add files via upload

---
+++ additionquiz.java
@@ -0,0 +1,25 @@
+package scr;
+
+import java.util.Scanner;
+public class additionquiz {
+	public static void main(String[] args){
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		
+		//create a scanner
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(
+				"What is " + number1 + " + " + number2 + "? ");
+		
+		int answer = input.nextInt();
+		
+		System.out.println(
+				number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+		
+		
+	}
+	  
+	
+
+}
